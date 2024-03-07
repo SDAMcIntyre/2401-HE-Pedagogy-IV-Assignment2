e067af118d866b759bc3231da0395502a3804627
S McIntyre <sdamcintyre+git@gmail.com>
2024-03-07 13:58:00 +0100

    figure of communication channels

--- analyses.R
+++ analyses.R
@@ -3,6 +3,23 @@ library(tidyverse)
 
 raw_data <- read_excel("Survey on campus versus distance learning(1-43).xlsx")
 
+# from summary_results_colors.pdf
+tool_names <- c("Zoom video calls", "Email", "MS Teams video calls", "MS Teams text chats", "Other")
+communication_data <- tibble(
+  comm_tool = factor(tool_names, levels = rev(tool_names)),
+  Frequency = c(37,19,13,6,3)
+ ) %>% 
+  mutate(Percent = 100*Frequency/43)
+
+ggplot(
+  data = communication_data,
+  mapping = aes(x = Percent, y = comm_tool)
+) +
+  geom_col(fill = "#4DBBD5FF") +
+  scale_x_continuous(limits = c(0,100)) +
+  theme_classic(base_size = 16) +
+  labs(y = NULL, subtitle = "What communication channels do you use for online learning?")
+
 recommend_data <- raw_data %>% 
   mutate(
     Recommend = case_when(
@@ -29,3 +46,4 @@ chisq.test(recommend_data$Recommend, recommend_data$ManageTime)
 
 xtabs(~ Recommend + Motivated, recommend_data)
 chisq.test(recommend_data$Recommend, recommend_data$Motivated)
+
